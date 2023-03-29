c712a671ae090ed713eb986fe28ab0e48496f904
Anders Klever Kirkeby <anders@klever-kirkeby.com>
2023-03-29 13:11:23 +0200

    fix: update vscode extensions

--- configs/vscode/install-extensions.sh
+++ configs/vscode/install-extensions.sh
@@ -4,17 +4,14 @@ echo "Installing Visual Studio code extensions and config."
 
 code -v >/dev/null
 if [[ $? -eq 0 ]]; then
-    read -r -p "Do you want to install VSC extensions? [y|N] " configresponse
-    if [[ $configresponse =~ ^(y|yes|Y) ]]; then
-        ok "Installing extensions please wait..."
-        code --install-extensions hashicorp.terraform
+    echo "Installing extensions please wait..."
+    code --install-extension hashicorp.terraform
+    code --install-extension dracula-theme.theme-dracula
+    code --install-extension foxundermoon.shell-format
 
-        ok "Extensions for VSC have been installed. Please restart your VSC."
-    else
-        ok "Skipping extension install."
-    fi
+    echo "Extensions for VSC have been installed. Please restart your VSC."
 else
-    error "It looks like the command 'code' isn't accessible."
-    error "Please make sure you have Visual Studio Code installed"
-    error "And that you executed this procedure: https://code.visualstudio.com/docs/setup/mac"
+    echo "It looks like the command 'code' isn't accessible."
+    echo "Please make sure you have Visual Studio Code installed"
+    echo "And that you executed this procedure: https://code.visualstudio.com/docs/setup/mac"
 fi
