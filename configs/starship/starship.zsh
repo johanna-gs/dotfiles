2867ec3b0aa6d4204a88ea759c3ea91b0bfdf22b
Anders Klever Kirkeby <anders.klever.kirkeby@statnett.no>
2023-11-24 09:27:17 +0100

    fix: starship fonts on macos

--- configs/starship/starship.zsh
+++ configs/starship/starship.zsh
@@ -8,7 +8,7 @@ if [[ -f $MFILE ]]; then
     _device=$(system_profiler SPHardwareDataType | awk '/Model Name/ {print $3,$4,$5,$6,$7}')
 
     case $_device in
-    *MacBook*) DEVICE="" ;;
+    *MacBook*) DEVICE="" ;;
     *mini*) DEVICE="󰇄" ;;
     *) DEVICE="" ;;
     esac
@@ -40,7 +40,7 @@ case $_distro in
 *devuan*) ICON="" ;;
 *manjaro*) ICON="" ;;
 *rhel*) ICON="" ;;
-*macos*) ICON="" ;;
+*macos*) ICON="" ;;
 *) ICON="" ;;
 esac
 
