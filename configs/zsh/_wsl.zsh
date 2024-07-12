14179d810732a0569b5a386d50df91b7331b0945
Johannes Schaanning <johannes.schaanning@statnett.no>
2024-07-12 09:44:49 +0200

    Remove INSERT at top right of terminal

--- configs/zsh/_wsl.zsh
+++ configs/zsh/_wsl.zsh
@@ -17,12 +17,6 @@
 eval $(keychain --eval --agents ssh id_rsa)
 
 # https://www.reddit.com/r/zsh/comments/sbt9zn/annoying_problem_with_starship_zsh_and_vimode/
-function zle-line-init zle-keymap-select {
-    RPS1="${${KEYMAP/vicmd/-- NORMAL --}/(main|viins)/-- INSERT --}"
-    RPS2=$RPS1
-    zle reset-prompt
-}
-
 zle -N zle-line-init
 zle -N zle-keymap-select
 
