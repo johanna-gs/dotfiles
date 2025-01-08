2138aae3f9157fb6269406e47c4bc9ee2b63f903
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-01-08 11:45:28 +0100

    update setup for ubuntu 24.04

--- configs/zsh/_functions.zsh
+++ configs/zsh/_functions.zsh
@@ -4,11 +4,11 @@ RED='\033[0;32m'
 NC='\033[0m' # No Color
 
 # Utility functions
-functions command_exists() {
+command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
-functions reload() {
+reload() {
     if [ -f  $HOME/.zshenv ]; then
         source $HOME/.zshrc
         clear
@@ -16,7 +16,7 @@ functions reload() {
     fi
 }
 
-functions kubectl_exec_into_pod() {
+kubectl_exec_into_pod() {
     if [ $# -eq 0 ]; then
         echo "No pod name supplied in arguments"
         return
@@ -26,7 +26,7 @@ functions kubectl_exec_into_pod() {
 }
 
 
-functions kubectl_psql_start() {
+kubectl_psql_start() {
     PGPASSWORD_POSTGRES=$(kubectl get secret --namespace default timescaledb-credentials -o jsonpath="{.data.PATRONI_SUPERUSER_PASSWORD}" | base64 --decode)
 
     kubectl run -i --tty --rm psql --image=postgres \
@@ -34,7 +34,7 @@ functions kubectl_psql_start() {
         --command -- psql -U postgres -h timescaledb.default.svc.cluster.local postgres
 }
 
-functions docker_exec_into_container() {
+docker_exec_into_container() {
     if [ $# -eq 0 ]; then
         echo "No container name supplied in arguments"
         return
@@ -42,7 +42,3 @@ functions docker_exec_into_container() {
     echo "Execing into container $1";
     docker exec -i -t $1 /bin/bash
 }
-
-#functions fix_wsl_clock() {
-#    sudo ntpdate pool.ntp.org
-#}
--- configs/zsh/_wsl.zsh
+++ configs/zsh/_wsl.zsh
@@ -1,25 +1,7 @@
 #!/usr/bin/env zsh
 
-# Set Windows display for WSL
-# export DISPLAY=':0.0'
-# export LIBGL_ALWAYS_INDIRECT=1
-# export GDK_BACKEND=x11
-
-# # Oracle Tools
-# export PATH=$PATH:$HOME/.local/lib/oracle/instantclient_19_18
-# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib/oracle/instantclient_19_18
-# export ORACLE_HOME=$HOME/.local/lib/oracle/instantclient_19_18
-# export TNS_ADMIN=~/tnsadmin
-
-# # OCI
-# export OCI_CLI_SUPPRESS_FILE_PERMISSIONS_WARNING=True
-
-eval $(keychain --eval --agents ssh id_rsa)
+eval $(keychain --eval --agents ssh github)
 
 # https://www.reddit.com/r/zsh/comments/sbt9zn/annoying_problem_with_starship_zsh_and_vimode/
 zle -N zle-line-init
 zle -N zle-keymap-select
-
-# if [ -d "/home/linuxbrew/.linuxbrew" ]; then
-#   export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-# fi
