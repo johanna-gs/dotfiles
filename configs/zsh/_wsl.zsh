d4b9f29742b01fa7eeb6d2e43fd76348451b58c7
Johannes Schaanning <johannes.schaanning@statnett.no>
2025-01-10 10:00:32 +0100

    move ssh eval to zshrc

--- configs/zsh/_wsl.zsh
+++ configs/zsh/_wsl.zsh
@@ -1,7 +1,4 @@
 #!/usr/bin/env zsh
-
-eval $(keychain --eval --agents ssh github)
-
 # https://www.reddit.com/r/zsh/comments/sbt9zn/annoying_problem_with_starship_zsh_and_vimode/
 zle -N zle-line-init
 zle -N zle-keymap-select
