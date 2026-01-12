a556dc923c4e332eaf6d90609d790140bcf9eabe
Johanna Schaanning <johanna.schaanning@statnett.no>
2026-01-12 08:46:42 +0100

    feat(navigation): allow passing arg directly to goto fun

--- configs/zsh/functions/_navigation.zsh
+++ configs/zsh/functions/_navigation.zsh
@@ -3,13 +3,20 @@
 alias goto="go_to_repo"
 go_to_repo() {
   local selected
-  selected=$(ls -1 ~/github | \
-    fzf \
-      --height 40% \
-      --reverse \
-      --border \
-      --preview 'ls -AF --color=always ~/github/{}' \
-      --preview-window=right:50%)
+
+  # If an argument is provided, use it directly
+  if [[ -n $1 ]]; then
+    selected="$1"
+  else
+    # Otherwise, use fzf to select
+    selected=$(ls -1 ~/github | \
+      fzf \
+        --height 40% \
+        --reverse \
+        --border \
+        --preview 'ls -AF --color=always ~/github/{}' \
+        --preview-window=right:50%)
+  fi
 
   if [[ -n $selected ]]; then
     cd ~/github/"$selected" || return 1
